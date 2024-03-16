26e3da06f0c310ef8126cb5676e18b4d7d822043
vietvuxuvi <57837565+vietvuxuvi@users.noreply.github.com>
2024-03-16 22:35:26 +0700

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RN1Yq2su1KnmsTFNNQDSpGce8J436bsbzy.vpsg -p x --cpu 1
